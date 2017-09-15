e741ee50640965aa72feeb2033e9be60a8b9a004
Gatlin Johnson <gatlin@niltag.net>
2017-09-15 14:40:32 -0500

    package structure

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Router } from './src/components/Router';
+import { Route } from './src/components/Route';
+import * as actions from './src/actions';
+import { routerReducer } from './src/reducers';
+
+export {
+    Router,
+    Route,
+    actions,
+    routerReducer
+};
